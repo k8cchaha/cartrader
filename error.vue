80dba85712738c02511f5c47d8cd71c1c4c80696
Alex_Wang <k8cchaha@gmail.com>
2023-04-30 01:35:04 +0800

    add error handle

---
+++ error.vue
@@ -0,0 +1,18 @@
+<script setup>
+const handleError = () => {
+  navigateTo("/");
+};
+</script>
+
+<template>
+  <div class="flex h-screen justify-center items-center flex-col">
+    <h1 class="text-9xl">404</h1>
+    <p class="mt-7 text-4xl">Page not found</p>
+    <button
+      @click="handleError"
+      class="mt-7 rounded text-2xl px-7 py-4 bg-blue-400 text-white"
+    >
+      Go Back
+    </button>
+  </div>
+</template>
