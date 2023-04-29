3905daa5f2a2e60c0597219abcca4a48a84677fb
Alex_Wang <k8cchaha@gmail.com>
2023-04-29 23:42:00 +0800

    add car cards and routes

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<!-- <script setup>
+import NavBar from "../components/NavBar.vue";
+</script> -->
+
 <template>
-  <div>Hello</div>
+  <div>
+    <NavBar />
+    <CarHero />
+  </div>
 </template>
