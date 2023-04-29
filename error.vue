597bfaac20126e31e2ba83596264b8ee4421478e
Alex_Wang <k8cchaha@gmail.com>
2023-04-30 02:02:05 +0800

    read json file to dynamic show cars

--- error.vue
+++ error.vue
@@ -2,12 +2,14 @@
 const handleError = () => {
   navigateTo("/");
 };
+
+const error = useError();
 </script>
 
 <template>
   <div class="flex h-screen justify-center items-center flex-col">
-    <h1 class="text-9xl">404</h1>
-    <p class="mt-7 text-4xl">Page not found</p>
+    <h1 class="text-9xl">{{ error.statusCode }}</h1>
+    <p class="mt-7 text-4xl">{{ error.message }}</p>
     <button
       @click="handleError"
       class="mt-7 rounded text-2xl px-7 py-4 bg-blue-400 text-white"
