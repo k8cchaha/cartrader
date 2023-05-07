06fb96ef21b0801fc2a96f554a63f68478da5aef
Alex_Wang <k8cchaha@gmail.com>
2023-05-08 01:41:50 +0800

    add query for server api

--- server/api/carsdata.js
+++ server/api/carsdata.js
@@ -1,5 +1,8 @@
 import cars from '@/data/cars.json'
 
-export default defineEventHandler(() => {
-  return cars
+export default defineEventHandler((event) => {
+  const { q = ''} = getQuery(event)
+  return cars.filter((car)=>{
+    return car.name.toLowerCase().includes(q.toLowerCase())
+  })
 })
