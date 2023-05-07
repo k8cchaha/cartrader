e3246cf135f2edf16e8cbe4bfe8bcc548e6d817b
Alex_Wang <k8cchaha@gmail.com>
2023-05-08 02:10:19 +0800

    add vueuse module

---
+++ pages/apitest.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useNow } from '@vueuse/core'
+const now = useNow()
+import { refDebounced } from '@vueuse/core'
+const query = ref('')
+refDebounced(query, 1000)
+
+const { data, pending, error } = useFetch(()=>`/api/carsdata?q=${query.value}`)
+
+</script>
+
+<template>
+  <div>API Testing</div>
+  <input type="text" v-model="query" placeholder="Search">
+  <pre>{{ data }}</pre>
+  <div>Now : {{ now }}</div>
+</template>
